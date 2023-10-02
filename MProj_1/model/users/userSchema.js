d9ecdfd3b3425b3c067daba3b8109ff63aedf767
rihika <rihikajain.it25@jecrc.ac.in>
2023-10-02 10:33:49 +0530

    updates

---
+++ MProj_1/model/users/userSchema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  id: Number,
+  title: String,
+  description: String,
+  price: Number,
+  discountPercentage: Number,
+  rating: Number,
+  stock: Number,
+  brand: String,
+  category: String,
+  thumbnail: String,
+  images: [String],
+});
+
+const items1 = mongoose.model("items1", productSchema);
+
+export default items1;
